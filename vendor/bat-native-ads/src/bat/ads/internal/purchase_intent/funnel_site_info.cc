cb2dc927ce86e2e4a2930dd80452e490fb6d029c
Moritz Haller <mhaller@brave.com>
2020-02-05 10:12:27 +0000

    Add keyword-segment lists, funnel-site lists and classifier for Brave Ads purchase intent
    Refactor

---
+++ vendor/bat-native-ads/src/bat/ads/internal/purchase_intent/funnel_site_info.cc
@@ -0,0 +1,32 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#include "bat/ads/internal/purchase_intent/funnel_site_info.h"
+
+namespace ads {
+
+FunnelSiteInfo::FunnelSiteInfo() = default;
+
+FunnelSiteInfo::FunnelSiteInfo(
+    const std::vector<std::string>& segments,
+    const std::string& url_netloc,
+    const uint8_t weight)
+    : segments(segments),
+      url_netloc(url_netloc),
+      weight(weight) {}
+
+FunnelSiteInfo::FunnelSiteInfo(
+    const FunnelSiteInfo& info)
+    : segments(info.segments),
+      url_netloc(info.url_netloc),
+      weight(info.weight) {}
+
+FunnelSiteInfo::~FunnelSiteInfo() = default;
+
+}  // namespace ads
---
+++ vendor/bat-native-ads/src/bat/ads/internal/purchase_intent/purchase_intent_signal_info.h
@@ -0,0 +1,33 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BAT_ADS_INTERNAL_PURCHASE_INTENT_SIGNAL_INFO_H_
+#define BAT_ADS_INTERNAL_PURCHASE_INTENT_SIGNAL_INFO_H_
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace ads {
+
+struct PurchaseIntentSignalInfo {
+ public:
+  PurchaseIntentSignalInfo();
+  PurchaseIntentSignalInfo(
+      const uint64_t timestamp_in_seconds,
+      const std::vector<std::string>& segments,
+      const uint16_t weight);
+  PurchaseIntentSignalInfo(
+      const PurchaseIntentSignalInfo& info);
+  ~PurchaseIntentSignalInfo();
+
+  uint64_t timestamp_in_seconds;
+  std::vector<std::string> segments;
+  uint16_t weight = 0;
+};
+
+}  // namespace ads
+
+#endif  // BAT_ADS_INTERNAL_PURCHASE_INTENT_SIGNAL_INFO_H_
